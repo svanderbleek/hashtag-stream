e8f86e356d3d86e46e2ab39383d618f0d7abc9ec
Sandy Vanderbleek <sandy.vanderbleek@gmail.com>
2014-03-26 20:30:26 -0700

    Integrate twitter for statuses

--- handlers/stream.js
+++ handlers/stream.js
@@ -1,10 +1,16 @@
 var config = require('nconf'),
-  instagram = require('instagram-node-lib');
+  instagram = require('instagram-node-lib'),
+  Twitter = require('twitter');
 
 config.env();
 
 instagram.set('client_id', config.get('INSTAGRAM_ID'));
 instagram.set('client_secret', config.get('INSTAGRAM_SECRET'));
+
+twitter = new Twitter({
+  consumer_key: config.get('TWITTER_KEY'),
+  consumer_secret: config.get('TWITTER_SECRET')
+});
 
 var extractImage = function(media) {
   return media.images.low_resolution.url;
@@ -14,30 +20,50 @@ var extractImages = function(data) {
   return data.map(extractImage);
 }
 
-var imageRenderer = function(response, context) {
-  return function(data) {
-    var images = extractImages(data);
-    context.images = images;
-    console.log(context);
-    response.view('stream', context);
-  }
-}
-
 var pullImages = function(tag, complete) {
   instagram.tags.recent({
     name: tag,
-    complete: complete
+    complete: addToContextAndComplete('images', extractImages, complete)
   })
+}
+
+var extractStatus = function(status) {
+  return status['text'];
+}
+
+var extractStatuses = function(data) {
+  return data['statuses'].map(extractStatus);
+}
+
+var pullStatuses = function(tag, complete) {
+  twitter.search('#' + tag, addToContextAndComplete('statuses', extractStatuses, complete));
+}
+
+var addToContextAndComplete = function(property, valueFunction, complete) {
+  return function() {
+    var args = Array.prototype.slice.call(arguments);
+    var value = valueFunction.apply(this, args);
+    complete(property, value);
+  }
+}
+
+var responseRenderer = function(response, context) {
+  return function(property, value) {
+    context[property] = value;
+    if(context.statuses && context.images) {
+      response.view('stream', context);
+    }
+  }
 }
 
 var buildResponse = function(tag, response) {
   var context = {tag: tag};
-  renderImages = imageRenderer(response, context);
-  pullImages(tag, renderImages);
+  var renderer = responseRenderer(response, context);
+  pullImages(tag, renderer);
+  pullStatuses(tag, renderer);
 }
 
 exports.media = function(request, response) {
   var tag = request.query.tag;
   buildResponse(tag, response);
-  return false;
 }
